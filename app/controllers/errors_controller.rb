2760b6f005b481e67c4b4849b8ce22aedefd9742
Stefan Botzenhart <sb@ninjaconcept.com>
2015-03-12 00:31:11 +0100

    Update error pages to ninja style
    Taken from ninjaconcept website. closes #1

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,17 +1,17 @@
 class ErrorsController < ApplicationController
 
   def not_found
-    @page_title = "Seite wurde nicht gefunden (404)"
+    @page_title = "Page not found (404)"
     render :status => 404
   end
 
   def unacceptable
-    @page_title = "Interner Fehler (422)"
+    @page_title = "Something weird happened (422)"
     render :status => 422
   end
 
   def server_error
-    @page_title = "Interner Serverfehler (500)"
+    @page_title = "Internal server error (500)"
     render :status => 500
   end
 
